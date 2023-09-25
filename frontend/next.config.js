073a783d8fad35115b5e9eb84708b4cbbe50f700
heejinme <heejinna.dev@gmail.com>
2023-09-26 00:11:13 +0900

    chore: css 라이브러리 관련 설정 추가
    vanilla extract 관련 의존성 추가함
    next.js에 필요한 vanilla extract 플러그인 설정 추가함
    
    Related to: #1

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 
-module.exports = nextConfig
+const withVanillaExtract = createVanillaExtractPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = withVanillaExtract(nextConfig);
