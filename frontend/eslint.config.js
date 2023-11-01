e2f89a24f9fa91d9c404efe34f827335407e9a46
Heejin Na <heejinna.dev@gmail.com>
2023-11-01 14:33:23 +0900

    Feature/#1 project setting (#10)
    * chore: 패키지 모듈 설정 수정 (#1)
    
    * chore: ES Module로 설정 재변경 (#1)
    
    * chore: linter 및 formatter 관련 의존성 및 규칙 추가 (#1)
    
    ---------
    
    Co-authored-by: jiinaa <jina7430@gmail.com>

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,11 +1,106 @@
+/** @type {import('eslint').Linter.FlatConfig[]} */
+import nextPlugin from '@next/eslint-plugin-next';
+import reactPlugin from 'eslint-plugin-react';
+import hooksPlugin from 'eslint-plugin-react-hooks';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
 
 module.exports = [
   {
-    files: ['**/*.ts', '**/*.tsx'],
-    ignores: ['**/*.config.js', '**/*.config.ts'],
+    plugins: {
+      react: reactPlugin,
+    },
     rules: {
-      semi: 'error',
+      ...reactPlugin.configs['jsx-runtime'].rules,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    plugins: {
+      'react-hooks': hooksPlugin,
+    },
+    rules: hooksPlugin.configs.recommended.rules,
+  },
+  {
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+    },
+  },
+  {
+    ignores: ['.next/*'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: { modules: true },
+        ecmaVersion: 'latest',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      import: importPlugin,
+      '@typescript-eslint': tsPlugin,
+      tsPlugin,
+    },
+    settings: {
+      'import/parsers': {
+        espree: ['.js', '.cjs', '.mjs', '.jsx'],
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
+      },
+      'import/resolver': {
+        typescript: true,
+        node: true,
+      },
+    },
+    rules: {
+      ...tsPlugin.configs['eslint-recommended'].rules,
+      ...tsPlugin.configs['recommended'].rules,
+      // ...importPlugin.configs['recommended'].rules,
+      // 'ts/return-await': 2,
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal'],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'builtin',
+              position: 'after',
+            },
+            {
+              pattern: 'react-dom',
+              group: 'builtin',
+              position: 'after',
+            },
+            {
+              pattern: '*next/**',
+              group: 'builtin',
+              position: 'after',
+              patternOptions: {
+                partial: true,
+              },
+            },
+          ],
+          pathGroupsExcludedImportTypes: [],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
     },
   },
   eslintConfigPrettier,
---
+++ frontend/prettier.config.js
@@ -0,0 +1,17 @@
+/** @type {import('prettier').Config} */
+const prettierConfig = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
+
+export default prettierConfig;
