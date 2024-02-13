612783f8cbc6080a7314533f1f814745fd45a0db
jinaoh <jina7430@gmail.com>
2024-02-13 23:38:04 +0900

    feat: textarea, topic area 컴포넌트 상태 활성화 추가

--- frontend/src/app/write/page.tsx
+++ frontend/src/app/write/page.tsx
@@ -14,9 +14,18 @@ import Link from "next/link";
 export default function folderEdit() {
 
   const [isDone, setIsOpen] = useState(false);
-
+  const [isReadOnly, setReadOnly] = useState(false);
+  
+  // 수정, 저장 버튼 토글
   function toggleBtn(){
     setIsOpen((isDone) => !isDone);
+    setReadOnly((isReadOnly) => !isReadOnly);
+  }
+
+  // 토픽 닫기
+  const [isClose, setClose] = useState(true);
+  function closeTopic(){
+    setClose((isClose) => !isClose);
   }
 
   return (
@@ -33,20 +42,25 @@ export default function folderEdit() {
               <IconBtn name="" type="etc"></IconBtn>
               {!isDone && <button className="textBtn light" onClick={toggleBtn}>저장</button>}
               {isDone && <div className="headerBtnMin">
-                <button className="textBtn dark" onClick={toggleBtn}>수정</button>
-                <button className="textBtn dark">보내기</button>
+                <button type="button" className="textBtn dark" onClick={toggleBtn}>수정</button>
+                <button type="submit" className="textBtn dark">보내기</button>
               </div>}
             </div>            
           </header>
         </section>
-        <section className="topicWrap">
+        {isClose && <section className="topicWrap">
           <p className="topic">
           요즘 읽고 있는 책이 있나요? <br/> 또는 좋아하는 책에 대한 이야기를 들려주세요.
           </p>
-        </section>
+          <p className="icBtnWrap">
+            <button type="button" className="icBtn closeBtn" onClick={closeTopic}></button></p>
+        </section>}
         <section className="writeWrap">
-          <textarea placeholder="노트를 시작해주세요"></textarea> 
+          <textarea name="postContent" placeholder="노트를 시작해주세요" readOnly={isReadOnly}></textarea> 
         </section>
+      </div>
+      <div className="bottomSheet">
+        바텀시트 test
       </div>
     </div>
   );
