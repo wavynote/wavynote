d09edef80284c2c0f3cf3243e2d5c27f67b69565
jinaoh <jina7430@gmail.com>
2024-03-02 18:17:37 +0900

    refactor: 최상위 레이아웃 수정

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import Button from "@/components/Button";
 export default function HomePage() {
   return (
     <section className="h-full flex flex-col justify-evenly items-center px-4">
-      <div className="w-[293px] h-[293px] bg-zinc-300 rounded-full" />
+      {/* <div className="w-[293px] h-[293px] bg-zinc-300 rounded-full" />
       <h1 className="text-[26px] font-medium text-center">
         글쓰기가 마음건강에 도움이
         <br />
@@ -13,7 +13,7 @@ export default function HomePage() {
       <section className="flex flex-col gap-3">
         <Button name="웨이비노트 알아보기" theme="light" />
         <Button name="웨이비노트 시작하기" theme="dark" />
-      </section>
+      </section> */}
     </section>
   );
 }
