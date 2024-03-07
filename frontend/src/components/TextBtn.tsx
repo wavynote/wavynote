49a76563a0a2ce11fb2b252baa0de857a8b60a9f
jinaoh <jina7430@gmail.com>
2024-03-07 13:05:35 +0900

    feature: 로그인 마크업 및 스타일 진행

--- frontend/src/components/TextBtn.tsx
+++ frontend/src/components/TextBtn.tsx
@@ -3,6 +3,7 @@ type ButtonType = "light" | "dark" | "newNote";
 interface ButtonProps {
   name: string;
   type: ButtonType;
+  onClick?: () => void;
 }
 
 function getTheme(type: ButtonType) {
@@ -12,13 +13,23 @@ function getTheme(type: ButtonType) {
     case "dark":
       return "dark";
     case "newNote":
-      return "dark newNote"
+      return "dark newNote";
+    default:
+      return "";
   }
 }
 
-export default function Button({ name, type = "dark", ...rest }: ButtonProps) {
+
+
+export default function Button({ name, type = "dark", onClick, ...rest }: ButtonProps) {
+  const handleClick = () => {
+    if( onClick ){
+      onClick();
+    }  
+  }
+  
   return (
-    <button className={`textBtn ${getTheme(type)}`} {...rest}>
+    <button className={`textBtn ${getTheme(type)}`} onClick={handleClick} {...rest}>
       {name}
     </button>
   );
